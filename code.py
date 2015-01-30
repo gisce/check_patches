4af3e80a75a8ef8b96f774f5ac50cc147dc3a314
Eduard Carreras <ecarreras@gisce.net>
2015-01-30 13:57:54 +0100

    Show applied patches

--- code.py
+++ code.py
@@ -1,20 +1,29 @@
+from collections import OrderedDict
+
 from fabric.api import *
+
 
 def status_dict(directory):
     with cd(directory):
-        status = {}
+        status = OrderedDict()
         x = run("git describe --tags").split('-')
-        labels = ['version', 'patches', 'last_commit']
+        labels = ['version', 'num patches', 'last_commit']
         for field, label in zip(x, labels):
             status[label] = field
         if 'last_commit' in status:
             commit_hash = status['last_commit'][1:]
-            log = run("git log -n1 --format=%%s %(commit_hash)s"
+            log = run("git log -n1 --oneline %(commit_hash)s"
             % locals(), pty=False).strip()
             status['last_commit'] = {
                 'hash': commit_hash,
                 'log': log
             }
+        if 'num patches' in status:
+            logs = run("git log --no-merges --format=%h%s "
+            "{version}..{last_commit[hash]}".format(**status), pty=False)
+            status['applied patches'] = OrderedDict()
+            for log in logs.split('\n'):
+                status['applied patches'][log[:7]] = log[7:]
         return status
 
 def pretty(d, depth=0):
